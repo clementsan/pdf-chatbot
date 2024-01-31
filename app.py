3bf358086eaf8f531111b8011178fefc92e91fa6
clementsan <cvachet@sci.utah.edu>
2024-01-31 12:55:48 -0700

    Remove Yi and open-llama models

--- app.py
+++ app.py
@@ -23,9 +23,9 @@ import accelerate
 
 # default_persist_directory = './chroma_HF/'
 list_llm = ["mistralai/Mixtral-8x7B-Instruct-v0.1", "mistralai/Mistral-7B-Instruct-v0.2", "mistralai/Mistral-7B-Instruct-v0.1", \
-    "HuggingFaceH4/zephyr-7b-beta", "01-ai/Yi-6B-Chat-8bits", "VMware/open-llama-7b-v2-open-instruct", \
-    "meta-llama/Llama-2-7b-chat-hf", "microsoft/phi-2", "TinyLlama/TinyLlama-1.1B-Chat-v1.0", \
-    "mosaicml/mpt-7b-instruct", "tiiuae/falcon-7b-instruct", "google/flan-t5-xxl"
+    "HuggingFaceH4/zephyr-7b-beta", "meta-llama/Llama-2-7b-chat-hf", "microsoft/phi-2", \
+    "TinyLlama/TinyLlama-1.1B-Chat-v1.0", "mosaicml/mpt-7b-instruct", "tiiuae/falcon-7b-instruct", \
+    "google/flan-t5-xxl"
 ]
 list_llm_simple = [os.path.basename(llm) for llm in list_llm]
 
