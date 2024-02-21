ce888d87ac6db17ac40cfdaad9849d4d9ce4e416
clementsan <cvachet@sci.utah.edu>
2024-02-21 13:05:24 -0700

    Add new LLMs: gemma-2b-it and gemma-7b-it

--- app.py
+++ app.py
@@ -24,6 +24,7 @@ import accelerate
 
 # default_persist_directory = './chroma_HF/'
 list_llm = ["mistralai/Mixtral-8x7B-Instruct-v0.1", "mistralai/Mistral-7B-Instruct-v0.2", "mistralai/Mistral-7B-Instruct-v0.1", \
+    "google/gemma-7b-it","google/gemma-2b-it", \
     "HuggingFaceH4/zephyr-7b-beta", "meta-llama/Llama-2-7b-chat-hf", "microsoft/phi-2", \
     "TinyLlama/TinyLlama-1.1B-Chat-v1.0", "mosaicml/mpt-7b-instruct", "tiiuae/falcon-7b-instruct", \
     "google/flan-t5-xxl"
