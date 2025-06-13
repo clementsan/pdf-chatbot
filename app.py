015a2e09b258206c8eb4f192497ffad9a87a9826
Clement Vachet <1039042+clementsan@users.noreply.github.com>
2025-06-13 17:32:24 -0600

    Fix: gradio chatbot data type

--- app.py
+++ app.py
@@ -229,7 +229,7 @@ def gradio_ui():
                 qachain_btn = gr.Button("Initialize Question Answering chain")
 
         with gr.Tab("Step 4 - Chatbot"):
-            chatbot = gr.Chatbot(height=300)
+            chatbot = gr.Chatbot(height=300, type="messages")
             with gr.Accordion("Advanced - Document references", open=False):
                 with gr.Row():
                     doc_source1 = gr.Textbox(
